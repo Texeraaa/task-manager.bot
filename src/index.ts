164bdaed9eb77981baeeec1dab6313dcbe66b433
Texeraaa <pedro.henrique.ph2018@hotmail.com>
2025-01-31 00:57:52 -0300

    feat: making bot stay online

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-const text: string = "Hello World!!"
+import { ExtendClient } from './structs/ExtendedClient';
+export * from "colors"
 
-console.log(text)
+const client = new ExtendClient()
+
+client.start()
+
+export { client }
+
+client.on("ready", () => {
+    console.log("Bot online".green);
+    
+})
+
+client.on("messageCreate", (message) => {
+    if(message.author.id === client.user?.id) return
+
+    message.reply({
+        content: `${message.author.username} Epoca da bonança`
+    })
+})
---
+++ src/structs/ExtendedClient.ts
@@ -0,0 +1,19 @@
+import { Client, IntentsBitField, BitFieldResolvable, GatewayIntentBits, GatewayIntentsString, Partials } from "discord.js";
+import dotenv from "dotenv"
+dotenv.config()
+
+export class ExtendClient extends Client {
+    constructor(){
+        super({
+            intents: Object.keys(IntentsBitField.Flags) as BitFieldResolvable<GatewayIntentsString,number>,
+            partials: [
+                Partials.Channel, Partials.GuildMember ,Partials.GuildScheduledEvent,
+                Partials.Message, Partials.Reaction, Partials.ThreadMember, Partials.User
+            ]
+        })
+    }
+
+    public start(){
+        this.login(process.env.BOT_TOKEN)
+    }
+}
